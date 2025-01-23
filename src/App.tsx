09bf157b07fba9d518dcb9284ec48adcf91a36bf
naitik7475 <naitik7475@gmail.com>
2025-01-24 00:45:58 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import MyNavbar from './components/Navbar';
-import HeroSection from './components/Hero';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Hero from './components/Hero';
+import Eligibility from './components/Eligibility';
+// Import other components...
 
 function App() {
   return (
-    <div>
-      <MyNavbar/>
-      <HeroSection/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Hero />} />
+          <Route path="eligibility" element={<Eligibility />} />
+          {/* Add other routes here */}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Eligibility.tsx
+++ src/components/Eligibility.tsx
@@ -1,1 +1,29 @@
 //To be done by Khushu
+import { Carousel } from "flowbite-react";
+import { Link } from 'react-router-dom';
+
+const Eligibility = () => {
+  return (
+    <div className="p-8">
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel>
+        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+      </Carousel>
+    </div>
+      <div className="mt-8 text-center">
+        <Link 
+          to="/how-to-apply" 
+          className="rounded-lg bg-blue-500 px-6 py-2 text-white hover:bg-blue-600"
+        >
+          Proceed to Application
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Eligibility;
